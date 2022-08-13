79feff530c316d59650f532fdc333ef923e1f7b4
Chika <seika850113@gmail.com>
2022-08-13 20:09:52 +0800

    Revert "Fix while project opening OnGeneratedCSProject has thrown null exception"
    This reverts commit cb1e4dc33de800a8c03bcfb89dd40bf9de79384f.

--- NuGetImporterForUnity/Packages/NuGet Importer/Editor/NuGetImporterSettings.cs
+++ NuGetImporterForUnity/Packages/NuGet Importer/Editor/NuGetImporterSettings.cs
@@ -16,7 +16,13 @@ namespace kumaS.NuGetImporter.Editor
         private static NuGetImporterSettings instance;
 
         [NonSerialized]
-        private static string projectSettingsPath = Application.dataPath.Replace("Assets", "ProjectSettings");
+        private static string projectSettingsPath;
+
+        [InitializeOnLoadMethod]
+        private static void SetProjectSettingsPath()
+        {
+            projectSettingsPath = Application.dataPath.Replace("Assets", "ProjectSettings");
+        }
 
         public static NuGetImporterSettings Instance
         {
