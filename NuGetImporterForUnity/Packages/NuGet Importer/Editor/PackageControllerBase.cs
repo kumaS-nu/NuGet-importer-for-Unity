3ce7901ee3fd5af5a626986e727bc24eb3553c66
kumaS-nu <kumas.kumachan@gmail.com>
2022-09-09 22:39:11 +0900

    Fix for Unity2019.

--- NuGetImporterForUnity/Packages/NuGet Importer/Editor/PackageControllerBase.cs
+++ NuGetImporterForUnity/Packages/NuGet Importer/Editor/PackageControllerBase.cs
@@ -262,7 +262,7 @@ namespace kumaS.NuGetImporter.Editor
         private void ManagedPluginProcess(Package package, string nugetPackagePath, string extractPath, string managedPath)
         {
             List<string[]> frameworkDictionary = FrameworkName.ALLPLATFORM;
-            var targetFramework = frameworkDictionary.FirstOrDefault(framework => framework.Contains(package.targetFramework));
+            var targetFramework = frameworkDictionary.FirstOrDefault(f=> f.Contains(package.targetFramework));
             List<string> frameworkList = FrameworkName.TARGET;
 
             if (targetFramework == default)
